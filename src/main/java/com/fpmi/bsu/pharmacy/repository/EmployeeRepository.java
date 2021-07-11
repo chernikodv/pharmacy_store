349fd7d14bcff7a13f7603d5c34b20e670d92dab
Dmitry Chernikov <dmitry.chernikov@hansecom.com>
2021-07-11 12:14:21 +0300

    Some random comment 2

--- src/main/java/com/fpmi/bsu/pharmacy/repository/EmployeeRepository.java
+++ src/main/java/com/fpmi/bsu/pharmacy/repository/EmployeeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>, JpaSpecificationExecutor<Employee> {
     // just a random comment
+    // just a random comment 2
 }
